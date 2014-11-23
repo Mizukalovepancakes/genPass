8685d2ed285497bf46a5f95f6f4b7eb09bb6169c
Mizukalovepancakes <mizukalovepancakes@yahoo.fr>
2014-11-23 10:31:25 +0100

    Now we can choose the len of the password

--- main.py
+++ main.py
@@ -2,13 +2,26 @@
 # -*- coding: utf-8 -*-
 import random
 import string
+import sys
 
-def Generer():
+if(len(sys.argv) != 2):
+	print('TAILLE DU MDP PAR DEFAUT')
+	taille = 9
+else:
+	try:
+		taille = int(sys.argv[1])
+		print "Taille du mdp : " + str(taille)
+	except:
+		taille = 9
+		print("ERREUR LORS DE LA CONVERSION")
+		print('TAILLE DU MDP PAR DEFAUT')
+
+def Generer(t):
 	password = ""
 	letter = list(string.ascii_lowercase)
 	LETTER = list(string.ascii_uppercase)
 	number = list(string.digits)
-	while(len(password) < 9):
+	while(len(password) < t):
 		random_choice = random.randint(1,3)
 		if(random_choice == 1):
 			letter_random = random.choice(letter)
@@ -18,4 +31,4 @@ def Generer():
 			letter_random = random.choice(number)
 		password += letter_random
 	print(password)
-Generer()
+Generer(taille)
